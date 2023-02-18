548398b7d9efc327e669d465862f8dc671a90ef1
IlyhaW <125214907+IlyhaW@users.noreply.github.com>
2023-02-18 17:52:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,9 +1,9 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    while(a != b) { 
-        if(a > b) 
-            a -= b; 
+    while (a != b) {
+        if (a > b)
+            a -= b;
         else 
             b -= a; 
     }
